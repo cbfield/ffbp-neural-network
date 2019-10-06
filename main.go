ef2032915f16ae0ccad1995a4fefb1eff6839464
Christopher Basketfield <chris.basketfield@gmail.com>
2019-10-06 12:23:22 -0800

    basic linear equation implementation

--- main.go
+++ main.go
@@ -4,6 +4,20 @@ import (
 	"fmt"
 )
 
+type node struct {
+	m float64
+	b float64
+}
+
 func main() {
 	fmt.Println("Hello World!")
+
+	var x float64 = 1.5
+	var node1 = node{m:2,b:0}
+	var y float64
+	
+	y = node1.m*x + node1.b
+
+	fmt.Println(y)
+
 }
